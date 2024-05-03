ad2e961c987ec522e9769ee1b73a7e55086363cf
s4chin-verma <shivamvarma346@gmail.com>
2024-05-04 05:29:10 +0530

    sign-up page

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -41,11 +41,11 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   pages: {
-    signIn: '/auth/signin',
-    signOut: '/auth/signout',
-    error: '/auth/error', // Error code passed in query string as ?error=
-    verifyRequest: '/auth/verify-request', // (used for check email message)
-    newUser: '/auth/new-user', // New users will be directed here on first sign in (leave the property out if not of interest)
+    signIn: '/signin',
+    signOut: '/signout',
+    error: '/error', // Error code passed in query string as ?error=
+    verifyRequest: '/verify-request', // (used for check email message)
+    newUser: '/new-user', // New users will be directed here on first sign in (leave the property out if not of interest)
   },
   session: {
     strategy: 'jwt',
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,28 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
-
 export { default } from 'next-auth/middleware';
+
+export const config = {
+  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
+};
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
+
   if (
     token &&
-    (url.pathname.startsWith('/auth/signin') ||
-      url.pathname.startsWith('/signup') ||
-      url.pathname.startsWith('/verify'))
+    (url.pathname.startsWith('/sign-in') ||
+      url.pathname.startsWith('/sign-up') ||
+      url.pathname.startsWith('/verify') ||
+      url.pathname === '/')
   ) {
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.redirect(new URL('/dashboard', request.url));
   }
-  return NextResponse.redirect(new URL('/', request.url));
-}
 
-export const config = {
-  matcher: [
-    '/auth/signin',
-    '/auth/signup',
-    '/',
-    '/dashboard/:path*',
-    '/verify/:path*',
-  ],
-};
+  if (!token && url.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
+
+  return NextResponse.next();
+}
